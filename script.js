a1b1ef828ae36e3e2c11c62c213b6a51a2a61ffe
adamkelsall <adam.kelsall25@gmail.com>
2017-03-20 02:34:02 +0000

    External CSV data source

--- script.js
+++ script.js
@@ -6,24 +6,31 @@ const graph = {
   height: 500
 };
 
-// Basic and arbitrary data points
-const data = [
-  { x: 25, y: 25 },
-  { x: 50, y: 75 },
-  { x: 100, y: 150 },
-  { x: 200, y: 300 }
-];
-
 // Create a SVG to be used as the graph
 const svg = d3.select('#graph').append('svg') // Add a SVG into the HTML element with ID 'graph'
   .attr('width', graph.width)                 // Set SVG width
   .attr('height', graph.height);              // Set SVG height
 
-// Add a dot to the SVG for each data point
-svg.selectAll('.dot')       // Select any existing elements with class 'dot'
-  .data(data)               // Add data from 'data' variable to the selection
-  .enter().append('circle') // For each selected piece of data, create a circle
-    .attr('class', 'dot')   // Give it the CSS class 'dot'
-    .attr('r', 3.5)         // Make its radius 3.5 pixels
-    .attr('cx', d => d.x)   // Use data 'x' value to position circle's centre on the X axis
-    .attr('cy', d => d.y);  // Use data 'y' value to position circle's centre on the Y axis
+// Read CSV data from an external data source
+// Once done, run the code in the callback function to use the data
+d3.csv('data/basic.csv', (error, data) => {
+
+  // If something went wrong, raise an error (code in this function will go no further)
+  if (error) throw error;
+
+  // CSV files only contain text
+  // Convert numeric strings in each data point into actual numbers
+  data.forEach((d) => {
+    d.x = Number(d.x);
+    d.y = Number(d.y);
+  });
+
+  // Add a dot to the SVG for each data point
+  svg.selectAll('.dot')       // Select any existing elements with class 'dot'
+    .data(data)               // Add data from 'data' variable to the selection
+    .enter().append('circle') // For each selected piece of data, create a circle
+      .attr('class', 'dot')   // Give it the CSS class 'dot'
+      .attr('r', 3.5)         // Make its radius 3.5 pixels
+      .attr('cx', d => d.x)   // Use data 'x' value to position circle's centre on the X axis
+      .attr('cy', d => d.y);  // Use data 'y' value to position circle's centre on the Y axis
+});
